506413e50c7b9c24ab4932d09885c3f97e52533c
Martin Catty <mcatty@synbioz.com>
2014-09-18 17:04:51 +0200

    define id as primary key, remove useless user_id.

--- db/setup.rb
+++ db/setup.rb
@@ -2,13 +2,12 @@ require_relative 'configuration'
 
 sql_create_schema = "
   CREATE TABLE posts (
-    id SERIAL UNIQUE
+    id SERIAL PRIMARY KEY
   );
   CREATE TABLE ratings (
-    id SERIAL UNIQUE,
+    id SERIAL PRIMARY KEY,
     score integer,
-    post_id integer,
-    user_id integer
+    post_id integer
   );
 "
 ActiveRecord::Base.connection.execute(sql_create_schema)
